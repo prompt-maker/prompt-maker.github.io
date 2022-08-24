8bd91a57e649f52d0915e2eca1a99147adf8daea
AlonS3 <64230372+AlonS3@users.noreply.github.com>
2022-08-24 12:36:14 +0300

    5

--- js/main.js
+++ js/main.js
@@ -129,7 +129,9 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     randomBtn.onclick = function(){
+        alert("test")
         seedInput.value = getRndInteger(1,9).toString() + getRndInteger(0,9).toString() + getRndInteger(0,9).toString() + getRndInteger(0,9).toString() + getRndInteger(0,9).toString() + getRndInteger(0,9).toString() + getRndInteger(0,9).toString() + getRndInteger(0,9).toString() + getRndInteger(0,9).toString() + getRndInteger(0,9).toString();
+        alert("ff")
     }
 
     function getRndInteger(min, max) {
